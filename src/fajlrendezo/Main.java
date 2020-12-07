838e972e8c2ffc4b03738e69267c997656096f39
Tibor Ignéczi <47614820+sidragosam@users.noreply.github.com>
2020-12-07 20:51:59 +0100

    Hungarian ➡️ English

--- src/fajlrendezo/Main.java
+++ src/fajlrendezo/Main.java
@@ -14,24 +14,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Fájrendező by Sidragosam");
+        System.out.println("File organizer by Sidragosam");
         Scanner beolvaso = new Scanner(System.in);
-        System.out.println("Írd be a mappa elérési helyét:");
+        System.out.println("Type or copy the directory's path:");
         String mappahely =  beolvaso.nextLine();
-        System.out.println("Beolvasás helye: " + mappahely);
+        System.out.println("The path is: " + mappahely);
         Path path = Paths.get(mappahely);
         File file = new File(mappahely);
         if(!Files.exists(file.toPath())){
-            System.err.println("Nem létezik ilyen mappa.");
+            System.err.println("No such directory exists.");
         }
         if(!Files.isDirectory(path)){
-            System.err.println("Ez nem egy mappa.");
+            System.err.println("This is not a directory.");
         }
         if(Files.isDirectory(path))
         {
             List<String> eredmeny = new ArrayList<>();
             search(".*", file, eredmeny);
-            System.out.println("Fájlok a mappában:");
+            System.out.println("Files in the directory:");
             for (String s : eredmeny) {
                 System.out.println(s);
             }
@@ -49,20 +49,20 @@ public class Main {
                     BasicFileAttributes attr = Files.readAttributes(f.toPath(), BasicFileAttributes.class);
                     String datum = attr./*creationTime()*/lastModifiedTime().toString();
                     String[] parts = datum.split("-");
-                    result.add(f.getAbsolutePath() + " Év: " + parts[0] + " Hónap: " + parts[1]);
+                    result.add(f.getAbsolutePath() + " Year: " + parts[0] + " Month: " + parts[1]);
                     File evMappa = new File(folder + "/"+parts[0]);
                     if (!evMappa.exists()){
                         evMappa.mkdirs();
-                        System.out.println(parts[0] + " mappa létrehozva. Elérés: " + evMappa.getPath());
+                        System.out.println(parts[0] + " directory created. Path: " + evMappa.getPath());
                     }
                     File honapMappa = new File(folder + "/"+parts[0]+"/"+parts[1]);
                     if (!honapMappa.exists()) {
                         honapMappa.mkdirs();
-                        System.out.println(parts[1] + " mappa létrehozva. Elérés: " + honapMappa.getPath());
+                        System.out.println(parts[1] + " directory created. Path: " + honapMappa.getPath());
                     }
                     //Files.move(f.toPath(), folder + "/"+parts[0]+"/"+parts[1]+f.toPath(), StandardCopyOption.REPLACE_EXISTING);
                     f.renameTo(new File(folder + "/"+parts[0]+"/"+parts[1]+"/"+f.getName()));
-                    System.out.println(f + " áthelyezve. Új elérése: " + folder + "/"+parts[0]+"/"+parts[1]+"/"+f.getName());
+                    System.out.println(f + " moved. New path: " + folder + "/"+parts[0]+"/"+parts[1]+"/"+f.getName());
                 }
             }
 
